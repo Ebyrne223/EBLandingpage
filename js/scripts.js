e87621bc51f439c2bbde88aaa1186e532c3a45ce
Ebyrne223 <145707559+Ebyrne223@users.noreply.github.com>
2023-12-11 05:06:41 +0000

    added html and css

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,8 @@
-// Check JS is Connected
-console.log('👍 JS Connected');
-
-// get all the card
-document.querySelectorAll('.card').forEach( item => {
-    
-    // for each item i.e. card, do this
-    item.addEventListener('click', event => {
-      
-      // toggle class on card
-      item.classList.toggle('flipped');
-    })
-  
-})
+function openNavbar() { 
+  document.getElementById("sideNavigationBar") 
+      .style.width = "50%"; 
+} 
+function closeNavbar() { 
+  document.getElementById("sideNavigationBar") 
+      .style.width = "0%"; 
+}
